34d5e4b1bf6ed11e52032e577ed4c9e7eaf8d9c4
daniel <daniel-congo2@hotmail.com>
2025-04-20 18:22:57 +0200

    to do: optimized code for me

--- index.js
+++ index.js
@@ -1,83 +1,87 @@
-let villeChoisie;
-let changerDeVille = document.querySelector('#changer');
+window.onload = function () {
+    const apiKey = "dc8c9152e8adaad0ec8bf635818c0d42";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + apiKey + "&";
 
-//Acces position utilisateur
-if ('geolocation' in navigator) {
-    navigator.geolocation.watchPosition((position)=>{//watch appelé à chaque fois que la position change (plus fiable que getCurrent position)
-        const url = 
-        'https://api.openweathermap.org/data/2.5/weather?lon=' + 
-        position.coords.longitude +'&lat='+ position.coords.latitude + 
-        '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
-        
-        let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-        requete.open('GET', url); // Nous récupérons juste des données
-        requete.responseType = 'json'; // Nous attendons du JSON
-        requete.send(); // Nous envoyons notre requête
-      
-        // Dès qu'on reçoit une réponse, cette fonction est executée
-        requete.onload = function() {
-          if (requete.readyState === XMLHttpRequest.DONE) {
-            if (requete.status === 200) {
-              let reponse = requete.response;
-              // console.log(reponse);
-              let temperature = reponse.main.temp;
-              let ville       = reponse.name;
-              // console.log(temperature);
-              document.querySelector('#temperature_label').textContent = temperature;
-              document.querySelector('#ville').textContent = ville;
-            }
-            else {
-              alert('Un problème est intervenu, merci de revenir plus tard.');
-            }
-          }
+    const searchBox = document.querySelector(".search input");
+    const searchBtn = document.querySelector(".search button");
+    const weatherIcon = document.querySelector(".weather-icon");
+
+    function updateWeatherInfos(data) {
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "img/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "img/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "img/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "img/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "img/mist.png";
         }
-    },error, options);
-}
-//Au cas où affiche par défaut Paris
-else{
-    alert('Impossible access location');
-    villeChoisie = 'Paris';
-    recevoirTemperature(villeChoisie);
-}
-//améliore la précision de la position
-var options = {
-    enableHighAccuracy:true
-}
-//position refusée affiche par défaut Paris
-function error() {
-    villeChoisie = 'Paris';
-    recevoirTemperature(villeChoisie);
-}
-//Changer de ville avec le boutton
-changerDeVille.addEventListener('click', () => {
-  villeChoisie = prompt('Which city do you want ?');
-  recevoirTemperature(villeChoisie);
-});
 
-function recevoirTemperature(ville) {
-  ville = ville.trim(); // ⚠️ enlève les espaces inutiles
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
-
-  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-  requete.open('GET', url); // Nous récupérons juste des données
-  requete.responseType = 'json'; // Nous attendons du JSON
-  requete.send(); // Nous envoyons notre requête
-
-  // Dès qu'on reçoit une réponse, cette fonction est executée
-  requete.onload = function() {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-      if (requete.status === 200) {
-        let reponse = requete.response;
-        // console.log(reponse);
-        let temperature = reponse.main.temp;
-        let ville       = reponse.name;
-        // console.log(temperature);
-        document.querySelector('#temperature_label').textContent = temperature;
-        document.querySelector('#ville').textContent = ville;
-      }
-      else {
-        alert("Enter a valid city please.");
-      }
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     }
-  }
-}
+
+    async function getWeatherByCity(city) {
+        const url = apiUrl + "q=" + city;
+        const response = await fetch(url);
+
+        if (response.status == 404) {
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".weather").style.display = "none";
+        } else {
+            const data = await response.json();
+            updateWeatherInfos(data);
+        }
+    }
+
+    function getWeatherByCoords(lat, lon) {
+        const url = apiUrl + "lat=" + lat + "&lon=" + lon;
+        let requete = new XMLHttpRequest();
+        requete.open("GET", url);
+        requete.responseType = "json";
+        requete.send();
+
+        requete.onload = function () {
+            if (requete.readyState === XMLHttpRequest.DONE && requete.status === 200) {
+                let data = requete.response;
+                updateWeatherInfos(data);
+            }
+        };
+    }
+
+    // Si l'utilisateur accepte la position
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                getWeatherByCoords(position.coords.latitude, position.coords.longitude);
+            },
+            (err) => {
+                alert("Vous avez refusé la position");
+                getWeatherByCity("Paris");
+            },
+            {
+                enableHighAccuracy: true,
+            }
+        );
+    } else {
+        getWeatherByCity("Paris");
+    }
+
+    // Rechercher une ville avec le bouton ou Entrée
+    searchBtn.addEventListener("click", () => {
+        getWeatherByCity(searchBox.value);
+    });
+
+    searchBox.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            getWeatherByCity(searchBox.value);
+        }
+    });
+};
